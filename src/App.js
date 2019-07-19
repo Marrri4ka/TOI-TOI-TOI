6c845b884ba1709cd08d5ff50a7bd4a489aa3a56
Mariia <mariiapopovych@gmail.com>
2019-07-19 09:47:59 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { Switch } from 'react-router-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+
 
 function App() {
   return (
   <div>
 
       <Header/>
+      <Footer/>
 
 
   </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  constructor(props) {
+    super(props);
+  };
+
+render(){
+return(
+
+<div>
+<footer class="page-footer font-small blue">
+
+
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  </div>
+
+
+</footer>
+</div>
+);
+}
+}
+
+export default Footer;
