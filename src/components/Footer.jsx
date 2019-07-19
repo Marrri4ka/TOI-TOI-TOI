70f475447f2eae36369f3bdcf0cf73059133c2bd
Mariia <mariiapopovych@gmail.com>
2019-07-19 10:00:13 -0700

    social buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,11 @@ return(
 <footer class="page-footer font-small blue">
 
 
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+  <div class="footer-copyright text-center py-3">© 2019 Copyright:
+    <a> Mariia Stashuk </a>
+    <a href="https://github.com/Marrri4ka" class="git-ic mr-3" role="button"><i class="fab fa-lg fa-github"></i></a>
+    <a  href="https://www.linkedin.com/in/mariia-stashuk/" class="li-ic mr-3" role="button"><i class="fab fa-lg fa-linkedin-in"></i></a>
+    <a  href="https://www.facebook.com/maria.popovytch" class="fb-ic mr-3" role="button"><i class="fab fa-lg fa-facebook-f"></i></a>
   </div>
 
 
