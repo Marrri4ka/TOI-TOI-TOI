2d8ba6654a447d2963ea7d45f8f251f114c7944e
Mariia <mariiapopovych@gmail.com>
2019-07-24 23:04:33 -0700

    Style ShippingMetod

--- src/components/ShippingMethod.jsx
+++ src/components/ShippingMethod.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {
+  Link
+} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function ShippingMethod(props){
-
+function ShippingMethod(props) {
   let _pickUp = null;
   let _seattleArea = null;
   let _explainRules = null;
 
-  function handleSaveShippingInfo(){
-    alert(_pickUp.checked);
-    props.saveShippingInfo(_pickUp.checked,_seattleArea.checked, _explainRules.checked);
-
+  function handleSaveShippingInfo() {
+    props.saveShippingInfo(_pickUp.checked, _seattleArea.checked, _explainRules.checked);
   }
-  return(
-    <div class="container">
-      	<div class="row">
-        	<div class="boxes">
-          <input type="checkbox" id="box-1" ref ={(input)=>{_pickUp=input;}}/>
+
+  return (
+    <div class="shippingForm">
+
+          <input type="checkbox" id="box-1" ref ={(input)=>{_pickUp=input;}} />
           <label for="box-1">Pick-up from store FREE </label>
 
           <input type="checkbox" id="box-2" ref ={(input)=>{_seattleArea=input;}} />
@@ -25,18 +24,19 @@ function ShippingMethod(props){
 
           <input type="checkbox" id="box-3" ref ={(input)=>{_explainRules=input;}}/>
           <label for="box-3">We can explain reules for extra $5</label>
-          <Link to='/address'><button onClick={()=> handleSaveShippingInfo()} class="btn btn-grey pull-left btn-fyi"><span class="glyphicon glyphicon-chevron-left"></span> Continue</button></Link>
-
-
-        </div>
-
-
-      	</div>
+          <Link to='/address'>
+            <button
+              onClick={()=> handleSaveShippingInfo()}
+              class="btn btn-grey pull-left btn-fyi">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              Continue
+            </button>
+          </Link>
     </div>
   );
 }
 
-ShippingMethod.propTypes={
+ShippingMethod.propTypes = {
   saveShippingInfo: PropTypes.func
 }
 
