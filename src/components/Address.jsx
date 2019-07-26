c0d8bcb29e7ca08d3038340378aa3760fb3aaf25
Mariia <mariiapopovych@gmail.com>
2019-07-25 22:18:16 -0700

    remove state

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -84,70 +84,16 @@ function Address(props){
                   </div>
               </div>
             </div>
+
             <div class="col-4">
               <div class="form-group">
-                <label class="col-md-4 control-label">State</label>
-                  <div class="col-md-4 selectContainer">
-                    <div class="input-group">
-                      <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
-                      <select name="state" class="form-control selectpicker" style={{width: 100 + 'px'}}>
-                        <option value=" " >Please select your state</option>
-                        <option>Alabama</option>
-                        <option>Alaska</option>
-                        <option >Arizona</option>
-                        <option >Arkansas</option>
-                        <option >California</option>
-                        <option >Colorado</option>
-                        <option >Connecticut</option>
-                        <option >Delaware</option>
-                        <option >District of Columbia</option>
-                        <option> Florida</option>
-                        <option >Georgia</option>
-                        <option >Hawaii</option>
-                        <option >daho</option>
-                        <option >Illinois</option>
-                        <option >Indiana</option>
-                        <option >Iowa</option>
-                        <option> Kansas</option>
-                        <option >Kentucky</option>
-                        <option >Louisiana</option>
-                        <option>Maine</option>
-                        <option >Maryland</option>
-                        <option> Mass</option>
-                        <option >Michigan</option>
-                        <option >Minnesota</option>
-                        <option>Mississippi</option>
-                        <option>Missouri</option>
-                        <option>Montana</option>
-                        <option>Nebraska</option>
-                        <option>Nevada</option>
-                        <option>New Hampshire</option>
-                        <option>New Jersey</option>
-                        <option>New Mexico</option>
-                        <option>New York</option>
-                        <option>North Carolina</option>
-                        <option>North Dakota</option>
-                        <option>Ohio</option>
-                        <option>Oklahoma</option>
-                        <option>Oregon</option>
-                        <option>Pennsylvania</option>
-                        <option>Rhode Island</option>
-                        <option>South Carolina</option>
-                        <option>South Dakota</option>
-                        <option>Tennessee</option>
-                        <option>Texas</option>
-                        <option> Uttah</option>
-                        <option>Vermont</option>
-                        <option>Virginia</option>
-                        <option >Washington</option>
-                        <option >West Virginia</option>
-                        <option>Wisconsin</option>
-                        <option >Wyoming</option>
-                      </select>
-                    </div>
+                <label class="col-md-4 control-label">Zip Code</label>
+                  <div class="input-group">
+                    <input  name="zip" placeholder="Zip Code" class="form-control"  type="text" />
                   </div>
               </div>
-            </div>
+            </div>            
+
           </div>
 
           <div class="row">
@@ -156,14 +102,6 @@ function Address(props){
                 <label class="col-md-4 control-label">Phone</label>
                   <div class="input-group">
                     <input name="phone" placeholder="(845) 555-1212" class="form-control" type="text"/>
-                  </div>
-              </div>
-            </div>
-            <div class="col-4">
-              <div class="form-group">
-                <label class="col-md-4 control-label">Zip Code</label>
-                  <div class="input-group">
-                    <input  name="zip" placeholder="Zip Code" class="form-control"  type="text" />
                   </div>
               </div>
             </div>
