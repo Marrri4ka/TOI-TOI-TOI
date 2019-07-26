76e212f868b7aa998df5d164e2d724c3ec08c851
Mariia <mariiapopovych@gmail.com>
2019-07-25 21:41:43 -0700

    add shipping style

--- src/components/ShippingMethod.jsx
+++ src/components/ShippingMethod.jsx
@@ -15,19 +15,19 @@ function ShippingMethod(props) {
 
   return (
     <div class="shippingForm">
-
+        <div class="formTitle"><h1>Shipping</h1></div><br/>
           <input type="checkbox" id="box-1" ref ={(input)=>{_pickUp=input;}} />
-          <label for="box-1">Pick-up from store FREE </label>
+          <label for="box-1"><h5>Pick-up from store FREE</h5></label>
 
           <input type="checkbox" id="box-2" ref ={(input)=>{_seattleArea=input;}} />
-          <label for="box-2">Seattle area $5 </label>
+          <label for="box-2"><h5>Seattle area $5</h5></label>
 
           <input type="checkbox" id="box-3" ref ={(input)=>{_explainRules=input;}}/>
-          <label for="box-3">We can explain reules for extra $5</label>
+          <label for="box-3"><h5>We can explain reules for extra $5</h5></label>
           <Link to='/address'>
             <button
               onClick={()=> handleSaveShippingInfo()}
-              class="btn btn-grey pull-left btn-fyi">
+              class="btn btn-grey btn-fyi btn-lg btn-block">
               <span class="glyphicon glyphicon-chevron-left"></span>
               Continue
             </button>
